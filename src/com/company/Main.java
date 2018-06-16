323a9d51c0020d371b73b66743cbb4bebd0f3ed0
cugoracle <l>
2018-06-16 11:36:41 +0800

    11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,9 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
         System.out.println("hello world5555555555555");
+        Thread.sleep(1221);
     }
 }
